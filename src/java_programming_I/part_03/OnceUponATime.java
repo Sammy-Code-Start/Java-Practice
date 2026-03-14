5717b8dad1978011dc3a0e2de562468a63a053ae
Adam-code-start <saksham.sap6@gmail.com>
2026-03-14 11:04:31 +0530

    I solved exercises 3 and 4

---
+++ src/java_programming_I/part_03/OnceUponATime.java
@@ -0,0 +1,10 @@
+package java_programming_I.part_03;
+
+public class OnceUponATime {
+    public static void main(String[] args)
+    {
+        System.out.println("Once upon a time");
+        System.out.println("there was");
+        System.out.println("a program");
+    }
+}
