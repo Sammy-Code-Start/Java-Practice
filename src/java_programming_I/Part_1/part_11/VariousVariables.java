7acc67cb6def32ab9648dc826c77b4cf5054cfbc
Adam-code-start <saksham.sap6@gmail.com>
2026-03-14 12:17:01 +0530

    I solved exercise 11

--- src/java_programming_I/Part_1/part_11/VariousVariables.java
+++ src/java_programming_I/Part_1/part_11/VariousVariables.java
@@ -1,5 +1,38 @@
 package java_programming_I.Part_1.part_11;
 
 public class VariousVariables {
+    public static void main(String[] args)
+    {
+        System.out.println("Chicken:");
+        int chicken_price = 3;
+        System.out.println(chicken_price);
+        System.out.println("Bacon (kg):");
+        double weight = 5.5;
+        System.out.println(weight);
+        System.out.println("Tractor:");
+        String message = "None!";
+        System.out.println(message);
+        System.out.println();
+        System.out.println("And finally, a summary:");
+        System.out.println(chicken_price);
+        System.out.println(weight);
+        System.out.println(message);
+
+        chicken_price = 9000;
+        weight = 0.1;
+        message = "Zero";
+        System.out.println();
+        System.out.println("Chicken:");
+        System.out.println(chicken_price);
+        System.out.println("Bacon (kg):");
+        System.out.println(weight);
+        System.out.println("Tractor:");
+        System.out.println(message);
+        System.out.println();
+        System.out.println("And finally, a summary:");
+        System.out.println(chicken_price);
+        System.out.println(weight);
+        System.out.println(message);
+    }
 
 }
