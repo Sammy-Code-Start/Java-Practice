5997e21c37505b4d59520780cbc4fcef836ffad7
Adam-code-start <saksham.sap6@gmail.com>
2026-03-14 11:52:40 +0530

    I solved exercise 10

---
+++ src/java_programming_I/part_10/Story.java
@@ -0,0 +1,17 @@
+package java_programming_I.part_10;
+import java.util.Scanner;
+public class Story {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("I will tell you as story, but I need some information first.");
+        System.out.println("What is the main character called?");
+        String name = sc.nextLine();
+        System.out.println("What is their job?");
+        String job = sc.nextLine();
+        System.out.println("Here is the story:");
+        System.out.println("Once upon a time there was " + name + ", who was " + job+".");
+        System.out.println("On the way to work, " + name + " reflected on life.");
+        System.out.println("Perhaps " + name + " will not be " + job + " forever.");
+    }
+}
