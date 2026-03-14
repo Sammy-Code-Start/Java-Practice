4d398c6a627dbd990c43f4cde7360e1518fdb3cd
Adam-code-start <saksham.sap6@gmail.com>
2026-03-14 11:14:51 +0530

    I solved exercise 6

---
+++ src/java_programming_I/part_06/HiAdaLovelace.java
@@ -0,0 +1,11 @@
+package java_programming_I.part_06;
+
+public class HiAdaLovelace {
+
+    public static void main(String[] args) {
+        String name = "Ada Lovelace";
+
+        System.out.println("Hi " + name + "!");
+
+    }
+}
