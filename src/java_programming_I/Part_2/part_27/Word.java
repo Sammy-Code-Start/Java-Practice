693ced56d40a77b63dc51b138d4066c9147ea284
Adam-code-start <saksham.sap6@gmail.com>
2026-03-23 18:02:39 +0530

    I have solved exercise 27

---
+++ src/java_programming_I/Part_2/part_27/Word.java
@@ -0,0 +1,15 @@
+package java_programming_I.Part_2.part_27;
+import java.util.Scanner;
+public class Word {
+    public static void main(String[] args)
+    {
+        System.out.println(word());
+    }
+
+    public static String word()
+    {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        return s;
+    }
+}
