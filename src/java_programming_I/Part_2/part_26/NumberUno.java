c47144c23854a25d66afbe958966e181204fb69d
Adam-code-start <saksham.sap6@gmail.com>
2026-03-23 17:55:41 +0530

    I have solved exercise 26

---
+++ src/java_programming_I/Part_2/part_26/NumberUno.java
@@ -0,0 +1,12 @@
+package java_programming_I.Part_2.part_26;
+import java.util.Scanner;
+public class NumberUno {
+    public static void main(String[] args)
+    {
+        numberUno();
+    }
+    public static int numberUno()
+    {
+        return 1;
+    }
+}
