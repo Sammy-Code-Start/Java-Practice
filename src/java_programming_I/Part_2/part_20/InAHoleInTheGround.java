e3335ac13969f03729efd46bfb85e6bd3328879c
Adam-code-start <saksham.sap6@gmail.com>
2026-03-23 17:25:20 +0530

    I solved exercise 20

---
+++ src/java_programming_I/Part_2/part_20/InAHoleInTheGround.java
@@ -0,0 +1,13 @@
+package java_programming_I.Part_2.part_20;
+import java.util.Scanner;
+public class InAHoleInTheGround {
+    public static void main(String[] args)
+    {
+        printText();
+    }
+
+    public static void printText()
+    {
+        System.out.println("In a hole in the ground there lived a method");
+    }
+}
