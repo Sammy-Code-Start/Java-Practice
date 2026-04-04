c11d778500c3fcbdbb55b6c08945a50c14c3e2e8
Adam-code-start <saksham.sap6@gmail.com>
2026-04-04 12:28:59 +0530

    I solved exercise 12

---
+++ src/java_programming_I/Part_3/part_12/SumOfAList.java
@@ -0,0 +1,40 @@
+package java_programming_I.Part_3.part_12;
+import java.util.Scanner;
+import java.util.ArrayList;
+public class SumOfAList {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Integer> numbers = new ArrayList<>();
+        while(true)
+        {
+            int num = Integer.valueOf(sc.nextLine());
+            if(num == -1)
+            {
+                break;
+            }
+
+            numbers.add(num);
+        }
+
+        //Using a for-each Loop
+        int sum = 0;
+        for(int x : numbers)
+        {
+            sum +=x;
+        }
+
+        System.out.println("Sum: " + sum);
+    }
+}
+
+/*
+import java.util.Scanner;
+import java.util.ArrayList;
+public class SumOfAList {
+    public static void main(String[] args)
+    {
+
+    }
+}
+ */
